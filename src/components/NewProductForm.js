fdaae1e2a15b1078597f0530da7eb615cacee5df
Paul Kasidit <paulkasidit@gmail.com>
2023-01-13 10:55:31 -0800

    finish all forms working on product controller

---
+++ src/components/NewProductForm.js
@@ -0,0 +1,34 @@
+import React from "react"; 
+import PropTypes from "prop-types"; 
+import ReusableForm from "./ReusableForm";
+import { v4 } from 'uuid'; 
+
+function NewProductForm(props){
+  
+  function handleNewProductFormSubmission(event){
+    event.preventDefault(); 
+    props.onNewProductCreation({
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      price: event.target.price.value,
+      roast: event.target.issue.value,
+      id: v4()
+    });
+  }
+
+  return(
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler= {handleNewProductFormSubmission}
+        buttonText = "Create New Product"
+      />
+    </React.Fragment>
+  )
+
+}
+
+NewProductForm.propTypes = {
+  onNewProductCreation: PropTypes.func
+}
+
+export default NewProductForm;
