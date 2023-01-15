c584af6119a110f876a40bebf92b7307bf1f7df2
Paul Kasidit <paulkasidit@gmail.com>
2023-01-15 10:46:07 -0800

    wrote first failing test for product list reducer

--- src/__tests__/reducers/product-list-reducer.test.js
+++ src/__tests__/reducers/product-list-reducer.test.js
@@ -1,7 +1,9 @@
 import ProductListReducer from '../../reducers/product-list-reducer';
 
 describe('ProductListReducer', () => {
+  
   test ("Should return default state if there is no action type passed into the reducer", () => {
     expect(ProductListReducer({},{type:null})).toEqual({});
   });
+
 });
