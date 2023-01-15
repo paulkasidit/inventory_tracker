b5f6f479b7f64b9124244ae343f0af32774feb7c
Paul Kasidit <paulkasidit@gmail.com>
2023-01-15 11:03:45 -0800

    wrote passing test and function for add product

--- src/__tests__/reducers/product-list-reducer.test.js
+++ src/__tests__/reducers/product-list-reducer.test.js
@@ -1,7 +1,40 @@
 import ProductListReducer from '../../reducers/product-list-reducer';
 
 describe('ProductListReducer', () => {
-  
+
+  let action; 
+  const productData = {
+    name: "Jamaica Blue",
+    origin: "Australia",
+    price: 3.00,
+    roast: "Dark Roast",
+    id: 1
+  };
+
+  test("Should add new product data to mainProductList", () => {
+    const {name , origin, price, roast, id} = productData;
+    
+    action = {
+      type: "ADD_PRODUCT", 
+      name: name,
+      origin: origin, 
+      price: price, 
+      roast: roast,
+      id: id
+    }; 
+
+    expect(ProductListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        origin: origin, 
+        price: price, 
+        roast: roast,
+        id: id
+      }
+    });
+
+  });
+
   test ("Should return default state if there is no action type passed into the reducer", () => {
     expect(ProductListReducer({},{type:null})).toEqual({});
   });
--- src/reducers/product-list-reducer.js
+++ src/reducers/product-list-reducer.js
@@ -1,5 +1,23 @@
 const reducer = (state = {}, action) => {
+  
+  const {name , origin, price, roast, id} = action;
+  
+  switch (action.type) {
 
+    case 'ADD_PRODUCT':
+      return Object.assign({}, state, {
+        [id]:{ 
+          name: name,
+          origin: origin, 
+          price: price, 
+          roast: roast,
+          id: id
+        }
+      });
+
+      default:
+        return state;
+  }
 };
 
 export default reducer;
